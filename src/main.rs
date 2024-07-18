46b023621dadc4b7c36b479fd0a91d0d30dd5c0b
Dmitriy Popov <dmitriiy.popov@cteleport.com>
2024-07-18 21:13:53 +0200

    jt4 stage

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use anyhow::anyhow;
 use flate2::read::ZlibDecoder;
+use flate2::write::ZlibEncoder;
+use flate2::Compression;
+use sha1::{Digest, Sha1};
 use std::env;
 use std::fs;
 use std::io;
-use std::io::Read;
+use std::io::{Read, Write};
 
 fn main() -> anyhow::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -14,6 +17,7 @@ fn process(args: &[String]) -> anyhow::Result<()> {
     match args[0].as_str() {
         "init" => handle_init_command()?,
         "cat-file" => handle_cat_file_command(&args[1..]),
+        "hash-object" => handle_hash_object_command(&args[1..])?,
         _ => println!("unknown command: {}", args[1]),
     };
     Ok(())
@@ -70,8 +74,12 @@ fn get_object_by_hash(hash: &str) -> anyhow::Result<Vec<u8>> {
     Ok(object_content)
 }
 
+fn get_object_dir_path(hash: &str) -> String {
+    ".git/objects/".to_string() + &hash[..2]
+}
+
 fn get_object_path(hash: &str) -> String {
-    ".git/objects/".to_string() + &hash[..2] + "/" + &hash[2..]
+    get_object_dir_path(hash) + "/" + &hash[2..]
 }
 
 fn get_decompressed_object(object_content: &[u8]) -> anyhow::Result<String> {
@@ -104,4 +112,46 @@ fn get_object_content(object: &str) -> anyhow::Result<String> {
         .skip_while(|&v| v != char::from(0x00))
         .skip(1)
         .collect())
+}
+
+fn handle_hash_object_command(args: &[String]) -> anyhow::Result<()> {
+    match args {
+        [] => println!("usage: git hash-object [-w] [--stdin] <file_name>"),
+        args => {
+            let file_name = args.last().expect("args are not empty");
+
+            let file_content = fs::read_to_string(file_name)?;
+            let blob = get_blob_content(&file_content);
+            let hash = get_object_hash(&blob);
+            println!("{hash}");
+
+            if args.contains(&"-w".to_string()) {
+                write_git_object(&hash, &blob)?;
+            }
+        }
+    };
+    Ok(())
+}
+
+fn get_blob_content(file_content: &str) -> String {
+    let size = file_content.len();
+    format!("blob {size}\0{}", file_content)
+}
+
+fn get_object_hash(object: &str) -> String {
+    let hasher = Sha1::digest(object);
+    hex::encode(hasher)
+}
+
+fn get_compressed_object(object: &str) -> anyhow::Result<Vec<u8>> {
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(object.as_bytes())?;
+    Ok(encoder.finish()?)
+}
+
+fn write_git_object(hash: &str, object: &str) -> anyhow::Result<()> {
+    let compressed = get_compressed_object(object)?;
+    fs::create_dir_all(get_object_dir_path(hash))?;
+    fs::write(get_object_path(hash), compressed)?;
+    Ok(())
 }
