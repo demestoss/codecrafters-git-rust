147330b0cad16a99226aaf40e646bbca15f4ba05
Tester Fraud <me_or_not@demestoss.com>
2024-07-29 12:27:16 +0200

    Commit command

--- src/commands/write_tree.rs
+++ src/commands/write_tree.rs
@@ -1,11 +1,13 @@
 use crate::objects::{Object, ObjectHash, ObjectKind};
-use anyhow::{anyhow, bail, Context};
-use std::fs;
+use anyhow::{bail, Context};
+use std::cmp::Ordering;
+use std::ffi::OsString;
 use std::fs::Metadata;
 use std::io::prelude::*;
 use std::io::Cursor;
 use std::os::unix::fs::PermissionsExt;
-use std::path::{Path, PathBuf};
+use std::path::Path;
+use std::{cmp, fs};
 
 pub fn handle() -> anyhow::Result<()> {
     let root = Path::new("./");
@@ -18,8 +20,7 @@ pub fn handle() -> anyhow::Result<()> {
 }
 
 pub fn write_tree_for(file_path: &Path) -> anyhow::Result<Option<ObjectHash>> {
-    let mut buf = Vec::new();
-    generate_tree_object(&file_path, &mut buf)?;
+    let buf = generate_tree_object(&file_path)?;
 
     if buf.is_empty() {
         Ok(None)
@@ -44,36 +45,58 @@ fn write_blob(file_path: &Path) -> anyhow::Result<ObjectHash> {
     Ok(hash)
 }
 
-fn generate_tree_object(file_path: &Path, mut buf: impl Write) -> anyhow::Result<()> {
+fn generate_tree_object(file_path: &Path) -> anyhow::Result<Vec<u8>> {
     let mut dir =
         fs::read_dir(&file_path).with_context(|| format!("read {}", file_path.display()))?;
     let mut entries = Vec::new();
     while let Some(res) = dir.next() {
         let res = res.context("incorrect dir entry")?;
         let file_name = res.file_name();
-        let name = file_name
-            .to_str()
-            .ok_or(anyhow!("error: file path is broken"))?;
         let meta = res.metadata().context("get path entry metadata")?;
         let path = res.path();
 
-        if is_path_ignored(name) {
+        if is_path_ignored(&file_name) {
             continue;
         }
-
-        let mode = get_mode(meta);
-
-        entries.push(TreeEntry {
-            mode,
-            name: name.to_owned(),
-            path,
-        })
+        entries.push((file_name, meta, path))
     }
 
-    entries.sort_by(|x, y| x.name.cmp(&y.name));
+    entries.sort_by(|a, b| {
+        // https://github.com/git/git/blob/e09f1254c54329773904fe25d7c545a1fb4fa920/tree.c#L128
+        let a_name = a.0.as_encoded_bytes();
+        let b_name = b.0.as_encoded_bytes();
+        let common_len = cmp::min(a_name.len(), b_name.len());
 
-    for TreeEntry { mode, name, path } in entries {
-        let is_dir = mode == "40000";
+        match a_name[..common_len].cmp(&b_name[..common_len]) {
+            Ordering::Equal => {}
+            o => return o,
+        }
+        if a_name.len() == b_name.len() {
+            return Ordering::Equal;
+        }
+
+        let c1 = if let Some(&c) = a_name.get(common_len) {
+            Some(c)
+        } else if a.1.is_dir() {
+            Some(b'/')
+        } else {
+            None
+        };
+        let c2 = if let Some(&c) = b_name.get(common_len) {
+            Some(c)
+        } else if b.1.is_dir() {
+            Some(b'/')
+        } else {
+            None
+        };
+        c1.cmp(&c2)
+    });
+
+    let mut buf = Vec::new();
+
+    for (name, meta, path) in entries {
+        let mode = get_mode(&meta);
+        let is_dir = meta.is_dir();
 
         let hash = if is_dir {
             let Ok(hash) = write_tree_for(&path) else {
@@ -85,21 +108,17 @@ fn generate_tree_object(file_path: &Path, mut buf: impl Write) -> anyhow::Result
         };
 
         if let Some(hash) = hash {
-            write!(buf, "{mode} {name}\0")?;
+            write!(buf, "{mode} ")?;
+            buf.extend(name.as_encoded_bytes());
+            write!(buf, "\0")?;
             buf.write(&hash)?;
         }
     }
 
-    Ok(())
+    Ok(buf)
 }
 
-struct TreeEntry {
-    name: String,
-    mode: String,
-    path: PathBuf,
-}
-
-fn get_mode(meta: Metadata) -> String {
+fn get_mode(meta: &Metadata) -> String {
     if meta.is_dir() {
         "40000".to_string()
     } else if meta.is_symlink() {
@@ -111,8 +130,12 @@ fn get_mode(meta: Metadata) -> String {
     }
 }
 
-const IGNORED_PATHS: &[&str; 3] = &[".git", "target", "debug"];
+const IGNORED_PATHS: &[&str; 4] = &[".git", "target", "debug", ".idea"];
 
-fn is_path_ignored(name: &str) -> bool {
-    IGNORED_PATHS.contains(&name)
+fn is_path_ignored(name: &OsString) -> bool {
+    if let Some(name) = name.to_str() {
+        IGNORED_PATHS.contains(&name)
+    } else {
+        true
+    }
 }
